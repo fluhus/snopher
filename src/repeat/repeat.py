4fbe53b6ce496441d6b0a4253ab1a29bf7d708f1
fluhus <doctor_troll@walla.co.il>
2020-10-27 17:28:56 +0200

    Format repeat.py

--- src/repeat/repeat.py
+++ src/repeat/repeat.py
@@ -3,13 +3,17 @@ import ctypes
 lib = ctypes.CDLL('./repeat.dll')
 repeat = lib.repeat
 
-repeat.argtypes = [ctypes.c_char_p, ctypes.c_longlong,
-                ctypes.c_char_p, ctypes.c_longlong]
+repeat.argtypes = [
+    ctypes.c_char_p,
+    ctypes.c_longlong,
+    ctypes.c_char_p,
+    ctypes.c_longlong,
+]
 repeat.restype = ctypes.c_char_p
 
 # Reusable output buffer.
 buf_size = 1000
-buf = (ctypes.c_char * buf_size)(*([0]*buf_size))
+buf = (ctypes.c_char * buf_size)(*([0] * buf_size))
 
 result = repeat(b'Badger', 4, buf, buf_size)  # type(result) = bytes
 print('Badger * 4 =', result.decode())
